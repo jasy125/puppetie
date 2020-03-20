13d6f7d0f1337a1bf0da3ef5bf24d0cd18c5aff5
root <admin@puppet.com>
2020-03-20 13:19:10 +0000

    test

--- tasks/adagentinstaller.ps1
+++ tasks/adagentinstaller.ps1
@@ -103,7 +103,7 @@ if ($computers.DNSHostName -ne "") {
        
         if (Get-service puppet -ErrorAction SilentlyContinue) {
             $puppetinstalled = Get-WmiObject Win32_Product | Where-Object { $_.Name -Like "Puppet*"}   | Select-Object Name,Version
-            return "Puppet Already Installed on $compname - ( Puppet: $($puppetinstalled.Name) Version: $($puppetinstalled.version)"
+            return "Puppet Already Installed on $compname - ( Puppet: $($puppetinstalled.Name) Version: $($puppetinstalled.version) )"
         } else {
             [System.Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; 
             [Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}; 
